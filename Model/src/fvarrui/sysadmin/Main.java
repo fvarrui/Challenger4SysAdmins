0bb066516f7f7a0685a76bc43ad9d7417e793031
fvarrui <4354486+fvarrui@users.noreply.github.com>
2018-03-30 16:33:09 +0100

    prueba ejecucion comando larga duracion en linux

--- Model/src/fvarrui/sysadmin/Main.java
+++ Model/src/fvarrui/sysadmin/Main.java
@@ -56,7 +56,7 @@ public class Main {
 //		ExecutionResult result = c.execute(false); 
 //		System.out.println(result);
 		
-		Process p = Runtime.getRuntime().exec("/bin/bash -c \"ping 8.8.8.8\"");
+		Process p = Runtime.getRuntime().exec("/usr/bin/sysdig -c spy_users");
 		BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
 		String line = null;
 		int i = 1;
