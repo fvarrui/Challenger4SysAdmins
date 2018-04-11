b23b1eedf5b9ebe8df67fcb895a192f92225952c
fvarrui <4354486+fvarrui@users.noreply.github.com>
2018-04-11 13:14:17 +0100

    arreglado error en script para quitar monitorizacion en linux

--- Challenger/config/scripts/linux/config-linux-monitoring.sh
+++ Challenger/config/scripts/linux/config-linux-monitoring.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo -e "Configurando la monitorizacion de interpretes de comandos de Linux (BASH):\n"
+
 bashrc=/etc/bash.bashrc
 
 # añade la variable PROMPT_COMMAND a la configuración global de la BASH (afecta a todos los usuarios) para que se registre en syslog el último comando ejecutado
@@ -10,3 +12,5 @@ else
 	echo "Añadiendo variable PROMPT_COMMAND a $bashrc"
 	echo "export PROMPT_COMMAND='history -a && logger --tag Challenger4SysAdmins \"bash:\$USER:\$PWD:\$OLDPWD:\$(tail -n 1 \$HISTFILE)\"'" >> $bashrc
 fi
+
+echo -e "\nProceso completado"
